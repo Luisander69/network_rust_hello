0f898589893dd0c591f8b398da213d2b467333e9
luisander <stefaniukliubomyr@gmail.com>
2023-10-05 05:36:13 +0000

    Ended borrowing and lifetime topics

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 extern crate term;
 
 fn main() {
-    let s = String::from("Test");
-    heap_example(&mut s);
-    println!("{}", s);
+    
+
+
+
+
+    //let v1 = vec![1, 2, 3, 4];
+    //let v2 = vec![1, 2];
+    //println!("{:?}", longer_vector(&v1, &v2));
+
+
+    //let mut s = String::from("Test");
+    //heap_example(&mut s);
     //color_text();
 
 }
+fn longer_vector<'a>(x: &'a[i32], y :&'a[i32]) -> &'a[i32]{
+    if x.len() > y.len() {x} else {y}
+}
+
 fn heap_example(input:&mut String){
-    let mystr = input;
+    let mut mystr = input;
+    *mystr =  String::from("Test2");
     let _otherstr = mystr.clone();
+    
     println!("{}", mystr);
 }
 fn color_text(){
